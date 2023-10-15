3a119950ad2131438005643aaa065b35b8cbdf05
Juliano Sill <sill.juliano@gmail.com>
2023-10-15 17:02:24 -0300

    [EN-004] feat(users): create addUser function

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,8 +1,26 @@
-import AppError from '../utils/AppError'
-import { database } from '../database/knex'
+import AppError from '../utils/AppError.js'
+import { database } from '../database/knex/index.js'
+
+import pkgBcryptjs from 'bcryptjs'
+const { hash } = pkgBcryptjs
 
 export default class UserController {
-  async create(req, res) {}
+  async create(req, res) {
+    const { name, email, password } = req.body
+    const emailExists = await database('users').where({ email }).first()
+
+    if (emailExists) {
+      throw new AppError('E-mail is already registered.')
+    }
+
+    const hashedPassword = await hash(password, 8)
+    const user = { name, email, password: hashedPassword }
+    await database('users').insert(user)
+
+    return res
+      .status(201)
+      .json({ message: 'User has been created successfully.' })
+  }
 
   async update(req, res) {}
 
