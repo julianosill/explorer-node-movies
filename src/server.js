cb416baa5192a22ec4fa4167782b4cf744589428
Juliano Sill <sill.juliano@gmail.com>
2023-10-15 16:09:33 -0300

    [EN-003] feat(AppError): create error exceptions handler

--- src/server.js
+++ src/server.js
@@ -3,5 +3,19 @@ import express from 'express'
 const api = express()
 api.use(express.json())
 
+api.use((error, req, res) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    })
+  }
+  console.log(error)
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  })
+})
+
 const PORT = 3333
 api.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
