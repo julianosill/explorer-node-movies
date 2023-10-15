f0a1677635b14263dd0340b4f7c19a7492e5fecc
Juliano Sill <sill.juliano@gmail.com>
2023-10-15 16:34:00 -0300

    [EN-004] feat(users): create UserController

---
+++ src/controllers/UserController.js
@@ -0,0 +1,10 @@
+import AppError from '../utils/AppError'
+import { database } from '../database/knex'
+
+export default class UserController {
+  async create(req, res) {}
+
+  async update(req, res) {}
+
+  async delete(req, res) {}
+}
