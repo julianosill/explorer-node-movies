01475c4af866112f5713473c393af58e36548433
Juliano Sill <sill.juliano@gmail.com>
2023-10-15 16:44:55 -0300

    [EN-004] fix: correct imports and set routes

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
+import 'express-async-errors'
+import AppError from './utils/AppError.js'
 import express from 'express'
+import routes from './routes/index.js'
 
 const api = express()
 api.use(express.json())
 
-api.use((error, req, res) => {
+api.use(routes)
+api.use((error, req, res, next) => {
   if (error instanceof AppError) {
     return res.status(error.statusCode).json({
       status: 'error',
