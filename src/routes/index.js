82016aea0c32cdc96b406d0367521cb0bc7259e5
Juliano Sill <sill.juliano@gmail.com>
2023-10-16 16:51:24 -0300

    [EN-007] build(routes): define main routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,12 @@
 import { Router } from 'express'
 
+import usersRoutes from './usersRoutes.js'
+import notesRoutes from './notesRoutes.js'
+import tagsRoutes from './tagsRoutes.js'
+
 const routes = Router()
+routes.use('/users', usersRoutes)
+routes.use('/notes', notesRoutes)
+routes.use('/tags', tagsRoutes)
 
 export default routes
